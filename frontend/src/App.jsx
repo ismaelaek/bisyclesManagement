34a0f8a8e8d73fe46286c603678609989686ddb6
Ismail Ait El Kamel <ismailaitelkamel@gmail.com>
2024-03-01 19:36:06 +0100

    Created the environment

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container border-2 border-black p-3 w-full'>
+      <h1 className='text-red-800 text-3xl'>Hello React + Vite + TailwindCSS </h1>
+    </div>
+  )
+}
+
+export default App
